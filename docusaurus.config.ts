3f1308baf1466769c17742c8154ee9af2144d304
hadziqmtqn <hadziqwebid@gmail.com>
2026-09-16 11:59:15 +0700

    Update favicon and logo SVGs; change favicon path in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'ERD Builder Pro Docs',
   tagline: 'Premium ERD Builder for Modern Developers',
-  favicon: 'img/app-logo.svg',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
